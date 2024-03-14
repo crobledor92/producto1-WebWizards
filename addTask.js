99ecd5f0684bc8fedbae2392beef028668f51ae8
B3LM0N <mbelmontesan@uoc.edu>
2024-03-14 16:49:09 +0100

    Int2_addTask

---
+++ addTask.js
@@ -0,0 +1,12 @@
+function addTask(event) {
+    event.preventDefault();
+    console.log(event.target.elements);
+    const taskData = {
+        title: event.target.elements.title.value,
+        description: event.target.elements.description.value,
+        endTime: event.target.elements.endTime.value,
+        participants: event.target.elements.participants.value,
+    }
+    console.log(taskData)
+
+}
